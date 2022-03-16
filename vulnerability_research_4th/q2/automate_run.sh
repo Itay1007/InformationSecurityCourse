dc33e2c563d7fa5c0c3552f82f75c40d189d3bdb
Itay Barok <itay.barok@gmail.com>
2022-03-16 14:58:59 +0200

    all files for vulnerability reshearch q2

---
+++ vulnerability_research_4th/q2/automate_run.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+rm core
+python3 q2b.py
+#sudo gdb --args ./sudo core
---
+++ vulnerability_research_4th/q2/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+void main()
+{
+   //execve("/bin/bash", 0, 0);
+    execve("/bin/bash", 0, 0);
+}
